44885da60f5957b95a9aafbb6d99f3859fc839b5
Johan Romero Rivera <jmromeror87@hotmail.com>
2025-09-30 18:50:13 -0500

    Create sum.kt

---
+++ sum.kt
@@ -0,0 +1,23 @@
+package com.example.jrkotlin
+
+/*
+Jhoan Romero
+Urenex Group LLC
+Systems Engineer
+suma10
+ */
+fun main() {
+    println("Digite el valor del numero 1:")
+    val numeroUno= readln().toInt()
+    println("Digite el valor del numero 2:")
+    val numeroDos= readln().toInt()
+    //val ResultadoSuma=numeroUno+numeroDos
+    //val ResultadoResta=numeroUno-numeroDos
+    //val ResultadoMul=numeroUno*numeroDos
+    val ResultadoDiv=numeroUno/numeroDos
+    //println("El Resultado de la Suma es : $ResultadoSuma")
+    //println("El Resultado de la Resta es : $ResultadoResta")
+    //println("El Resultado de la Multiplicacion es : $ResultadoMul")
+    println("El Resultado de la Divion es : $ResultadoDiv")
+}
+
